00969c166b85aa8820f809e7a3950ef0ea9acd64
cssnote <405468148@qq.com>
2020-09-02 05:36:28 +0800

    Update 整理client-offset-scroll.js

--- 整理client-offset-scroll.js
+++ 整理client-offset-scroll.js
@@ -1,5 +1,13 @@
-较极端情况：
-1、offsetLeft实际计算的偏移量是从元素的外边框(不含border)到html根元素（含border）的距离，包含了body的padding/margin/border,html根元素的padding/border,但不包含html的margin（ie11所有版本包含margin）.
-2、但是如果html或者body设置了定位（relative/position)，则计算到html和body的border（含border),IE8及以上版本不含border.
 
-一般情况都是body无border margin、html根元素无margin border padding的情况，就无须理会上述的问题了。
+client
+
+
+offset
+
+一、较极端情况：
+1、offsetLeft实际计算的偏移量是从元素的外边框（不含border）到html根元素内边框（含border）的距离，包含了body的padding/margin/border,html根元素的padding/border,但不包含html的margin（ie8及以上包含margin）.
+2、但是如果html或者body设置了定位（relative/position/fixed），则计算到html和body这两个offsetParent的border（含border）,IE8及以上版本不含border.
+
+二、一般情况都是body无border margin、html根元素无margin border padding的情况，就无须理会上述的问题了。
+
+三、如果元素的最近的父元素有定位，则偏移量是基于这个offsetParent，计算距离从元素的外边框到offsetParent的内边框，元素的margin + 中间元素的所有宽度 + offsetParent的padding.
